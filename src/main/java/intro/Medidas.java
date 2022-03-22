fd7615793e3eeff5428d4aa2580646fd808eb66c
TrillesP <trillessky@gmail.com>
2022-03-22 10:36:41 -0300

    Primeiro commit.
    Adicionando switch.
    
    Adicionando letras maiúsculas e minúsculas + scanner.
    
    +For com condições.
    
    Otimizando.
    
    Colocando menu em loop com opção de sair, adicionadas opções de escolha de números, adicionado tabuada.

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -8,35 +8,40 @@ import java.util.Scanner;
 // 3 - Classe
 public class Medidas {
     // 3.1 - Atributos - Características
-
+    static Scanner scanner = new Scanner(System.in);
     // 3.2 - Métodos e Funções
 
     public static void main(String[] args){
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Escolha uma opção.\n\nC - Calcular em modo compacto.\nE - Calcular em modo extenso.\nR - Contagem regressiva.\nD - Contar até 10.");
-        String opcao = scanner.next();
-        //String opcao = "blabla";
-        switch (opcao){
-            case "c":
-            case "C":
-                calcularAreaModoCompacto();
-                break;
-            case "E":
-            case "e":
-                calcularAreaModoExtenso();
-                break;
-            case "r":
-            case "R":
-                contagemRegressiva();
-                break;
-            case "d":
-            case "D":
-                contarAteDez();
-                break;
-            default:
-                System.out.println("Nenhum método válido escolhido.");
-                break;
+        String opcao = " ";
+        while (!opcao.equals("s") & !opcao.equals("S")) {
+            System.out.println("\nEscolha uma opção:\n");
+            System.out.println("1 - Calcular em modo compacto.\n2 - Calcular em modo extenso.\n3 - Contagem regressiva.\n4 - Contar até 10.\n5 - Tabuada.\nS - Sair.");
+            opcao = scanner.next();
+            //String opcao = "blabla";
+            switch (opcao) {
+                case "1":
+                    calcularAreaModoCompacto();
+                    break;
+                case "2":
+                    calcularAreaModoExtenso();
+                    break;
+                case "3":
+                    contagemRegressiva();
+                    break;
+                case "4":
+                    contarAteDez();
+                    break;
+                case "5":
+                    tabuada();
+                    break;
+                case "s":
+                case "S":
+                    System.out.println("Fechando programa.");
+                    break;
+                default:
+                    System.out.println("Nenhum método válido escolhido.");
+                    break;
+            }
         }
 
     }
@@ -46,9 +51,10 @@ public class Medidas {
         float largura;
         float comprimento;
         float resultado;
-
-        largura = 4;
-        comprimento = 3;
+        System.out.println("Digite a largura: ");
+        largura = scanner.nextFloat();
+        System.out.println("Digite o comprimento: ");
+        comprimento = scanner.nextFloat();
 
         resultado = largura * comprimento;
 
@@ -57,8 +63,10 @@ public class Medidas {
 
     public static void calcularAreaModoCompacto(){
         System.out.println("Calculando de modo compacto.");
-        int largura = 5;
-        int comprimento = 6;
+        System.out.println("Digite a largura: ");
+        float largura = scanner.nextFloat();
+        System.out.println("Digite o comprimento: ");
+        float comprimento = scanner.nextFloat();
         System.out.println("A área do retângulo é: " + largura*comprimento + " m2.");
     }
     public static void contarAteDez(){
@@ -73,5 +81,16 @@ public class Medidas {
             System.out.println(numero);
         }
     }
-
+    public static void tabuada(){
+        System.out.println("Escolha um número para ver sua tabuada: ");
+        int numero = scanner.nextInt();
+        for (byte i = 1; i<11; i++){
+            if (i == 10){
+                System.out.print((numero*i + "."));
+            }
+            else {
+                System.out.print(numero * i + ", ");
+            }
+        }
+    }
 }
